8f73fd6d6a9694b3aabf5a2c1e4be5a10b5436e5
Annisa878 <annisaoktavia878@gmail.com>
2025-06-11 14:20:04 +0700

    lulus

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -2,16 +2,11 @@ import Link from "next/link";
 import { ThemeSwitcher } from "../../components/theme-switcher";
 
 export default function Footer() {
-  return (
-    <footer className="w-full border-t border-sky-200 dark:border-sky-700 bg-blue-50 dark:bg-slate-800"> {/* Adjusted background and border */}
-      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center py-4 px-4">
-        <p className="text-xs text-white dark:text-sky-100">
-          © {new Date().getFullYear()} Perikanan Tangkap. All rights reserved.
-        </p>
-        <div className="mt-4 md:mt-0">
-          <ThemeSwitcher />
-        </div>
-      </div>
-    </footer>
-  );
+  const currentYear = new Date().getFullYear();
+  return (<footer className="w-full bg-gradient-to-r from-sky-500 to-cyan-600 text-white">
+    <div className="container mx-auto flex flex-col md:flex-row justify-between items-center py-4 px-4">
+      <p className="text-xs">© {currentYear} Perikanan Tangkap. All rights reserved.</p>
+      <div className="mt-4 md:mt-0"><ThemeSwitcher /></div>
+    </div>
+  </footer>);
 }
